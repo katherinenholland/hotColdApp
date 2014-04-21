78626c295624d034a8f1f6eb461c7da804e9c146
katherinenholland <katherinenholland@gmail.com>
2014-04-21 19:59:11 -0400

    Update
    Page Loads, Random Number is selected, Alerts the random number
    
    User CAN submit a guess that is logged.
    
    Problem: When the guess button is clicked, a new random number alert
    pops up.
    
    Things does not do:
    1. Log/give feedback
    2. Start a new game when "new game" button is clicked

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-
 $(document).ready(function(){
-	
+    secretNum;
+    alert(secretNum);
+    guess();
+});	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,6 +14,39 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-});
 
 
+var secretNum = 1 + Math.floor(Math.random() * 100); //when new game starts, a secret # from 1-100 is generated
+
+
+//reset = function(){
+	//$(".new").click(function(){
+     // location.reload();
+	//})
+//}//reload entire page to start new game when "new game" clicked
+
+guess = function(){
+    	$(".button").click(function(){
+    	$("#guessList").append("<li>" + $("#userGuess").val() + "</li>");
+    	$("#feedback").write(feedback);
+    	$("#count").write(feedback);
+    	$('#userGuess').val('');
+    })
+};
+
+var g = ("#userGuess").val();
+
+feedback = function(){
+   if(g == 10 + secretNum){
+   	write("Hot!!");
+   } else(g !== 10 + secretNum)
+   	write("Cold!!");
+   
+};
+	
+
+//var x = $("#userGuess").val();
+	//if(x <= 100) {
+	//guess();
+	//} else
+    //alert("Your guess must be a whole number between 1-100!"); //ensure number is between 1-100
