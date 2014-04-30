db92d9897bf7af800626ec6f0f6c76f78450276f
katherinenholland <katherinenholland@gmail.com>
2014-04-30 16:41:39 -0400

    All code is written, but it will not log feedback and guesses as required

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
-$(document).ready(function(){  
+$(document).ready(function() {  
     secretNum;
     alert(secretNum);
-    guess();
     feedback();
-    newGame();
-});	//end of document ready
+    guess();
+    $('#userGuess').val('');
+    //newGame();
 
+}); //end of document ready 
 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -23,36 +24,43 @@ $(document).ready(function(){
 var secretNum = 1 + Math.floor(Math.random() * 100); //when new game starts, a secret # from 1-100 is generated
 
 
-newGame = function(){
-	$(".new").click(function(){
-     location.reload();
-	})
-};//reload entire page to start new game when "new game" clicked
+
 
 guess = function(){
-      var g = $("#userGuess").val();
-    	$(".button").click(function(){
-    	$("#guessList").append("<li>" + g + "</li>");
-    	$("#feedback").text(feedback);
-    	$("#count").text(feedback);
-    	$('#userGuess').val('');
+      //var g = $("#userGuess").val();
+    	$(".button").click(function() {
+    	 $("#guessList").append("<li>" + $("#userGuess").val() + "</li>");
+        $("#guessList").append(feedback);
+    	   $("#feedback").append(feedback);
+    	   var counter = $("#feedback").val();
+          counter++;
+        $("#count").val(counter);
     })
 };//allows user guess to be logged
+
 
 feedback = function(){
    var g = $("#userGuess").val();
    if(g == secretNum) {
-   	return("Correct!!"); 
-   } else if(g >= 10 + secretNum) {
-   	return("Cold!!"); 
-   } else (guess <= 10 + secretNum) 
-    return("Hot!!");
+   	  return("Correct!!");
+    } else if (g % 1 != 0  && g <= 100 && g >= 1) {
+      return("Your guess must be a whole number between 1-100!")
+    } else if (g - secretNum >= 50) {
+      return("Ice Cold...Brrrrr.")
+    } else if (g - secretNum >= 30) {
+      return("Cold...")
+    } else if (g - secretNum >= 20) {
+      return("Warm...")
+    } else if(g - secretNum >= 10) {
+   	  return("Hot!");  
+    } else (g - secretNum < 10) 
+      return("Very Hot!!");
    
-}; //gives user feedback on guess
-	
+}; //gives user feedback on guess and checks to make sure a whole number 1-100
 
-//var x = $("#userGuess").val();
-	//if(x <= 100) {
-	//guess();
-	//} else
-    //alert("Your guess must be a whole number between 1-100!"); //ensure number is between 1-100
+newGame = function(){
+  $(".new").click(function(){
+     location.reload();
+  })
+};//reload entire page to start new game when "new game" clicked
+
